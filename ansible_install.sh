b7cdd530a4bc9a350d9773b6b219acb8ea992ea3
Scott Barning <ssbarning@gmail.com>
2023-12-25 14:14:13 -0500

    skip adding repo if it exists.

--- ansible_install.sh
+++ ansible_install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 #
 release_file=/etc/os-release
+source /etc/os-release
 
 sudo apt update
 sudo apt install software-properties-common python3-launchpadlib git -y
+
+if [ -f /etc/apt/sources.list.d/ansible-ubuntu-ansible-"$VERSION_CODENAME".list ]
+then
+    echo "Repository already exists, skipping."
+else
 sudo apt-add-repository ppa:ansible/ansible -y -n 
+fi
 
 # If the system this is running on is not Ubuntu, try to change the apt repository to one that will work.
 # e.g the Jammy repository for most recent Debian releases.
@@ -12,7 +19,6 @@ if grep -q "Ubuntu" $release_file
 then
     sudo apt update
 else
-    source /etc/os-release
     sudo sed -i "s/$VERSION_CODENAME/jammy/" /etc/apt/sources.list.d/ansible-ubuntu-ansible-"$VERSION_CODENAME".list
 fi
 
