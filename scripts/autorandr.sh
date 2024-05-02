2cd250cb1706fbc0a0c5c8ac9a9a819818de653f
Scott Barning <ssbarning@gmail.com>
2024-05-02 18:15:44 -0400

    updates

--- scripts/autorandr.sh
+++ scripts/autorandr.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-path="$(which autorandr)"
-
-if [ -f $path ]; then
-    autorandr -c
-fi
+xrandr --output DP-4 --primary  --output DP-2 --pos 0x0 --rotate right --left-of DP-4 --output DP-1 --pos 3360x560 --right-of DP-4 --output HDMI-0 --off
