f244ea08d5e559574e10c331a18b1030cb9278ea
BladeWDR <scott@ssb-tech.net>
2024-06-18 19:39:30 -0400

    some changes, idk lol

--- scripts/autorandr.sh
+++ scripts/autorandr.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 xrandr --output DP-4 --primary  --output DP-2 --pos 0x0 --rotate right --left-of DP-4 --output DP-1 --pos 3360x560 --right-of DP-4 --output HDMI-0 --off
