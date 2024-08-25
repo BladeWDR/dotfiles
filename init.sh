d2089fb250b06f3190f7635c15be57fb7fba4bb2
BladeWDR <scott@ssb-tech.net>
2024-08-25 14:39:09 -0400

    v1 of new init script.

---
+++ init.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+# source the release file so I can get the VERSION_CODENAME variable.
+. /etc/os-release
+
+# apps I want on every system, including servers.
+# Keep this to the VERY basics, as most things will be installed by the ansible playbook.
+BASICS=(
+        software-properties-gtk
+        python3-launchpadlib
+        git
+        zsh
+        zsh-autosuggestions
+        zsh-syntax-highlighting
+        tmux
+        direnv
+)
+
+# desktop apps that can be installed via apt
+DESKTOP_APPS_APT=(
+    i3
+    pulseaudio
+    pavucontrol
+    brightnessctl
+    pcscd
+)
+
+# Install dependencies.
+sudo apt update
+sudo apt install $BASICS
+
+# Check to see if the ansible repo already exists, if it does not, then add it.
+if [ -f "/etc/apt/sources.list.d/ansible-ubuntu-ansible-$VERSION_CODENAME.list" ]; then
+    echo "Repository already exists, skipping."
+else
+    sudo apt-add-repository ppa:ansible/ansible -y -n
+fi
+
+sudo apt update && sudo apt install ansible -y
+
+
+ANSIBLE_PATH=$(which ansible)
+
+if [ -f "$ANSIBLE_PATH" ]; then
+    echo "######################################################"
+    echo "Running ansible playbook."
+    echo "######################################################"
+    ansible-galaxy install -r staging/meta/requirements.yml --roles-path ./staging/roles
+    ansible-playbook -K ./staging/staging.yml
+else
+    echo "Error: Ansible not found."
+    echo "Something has gone horribly wrong."
+    exit 1
+fi
+
+read -p "Does this machine have a GUI?" DESKTOP_APPS_CHOICE
+
+if ![ $DESKTOP_APPS_CHOICE == "Y" ] || ![ $DESKTOP_APPS_CHOICE == "y" ]; then
+    echo "Skipping desktop apps."
+else
+    sudo apt install $DESKTOP_APPS_APT
+fi
