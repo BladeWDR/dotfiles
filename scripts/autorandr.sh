bb61dd685f18299012b71c50ccf382ed79ef248e
BladeWDR <scott@ssb-tech.net>
2024-11-30 13:58:11 -0500

    update display names for new GPU

--- scripts/autorandr.sh
+++ scripts/autorandr.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-xrandr --output DP-4 --primary  --output DP-2 --pos 0x0 --rotate right --left-of DP-4 --output DP-1 --pos 3360x560 --right-of DP-4 --output HDMI-0 --off
+xrandr --output DisplayPort-0 --primary  --output DisplayPort-1 --pos 0x0 --rotate right --left-of DisplayPort-0 --output DisplayPort-2 --pos 3360x560 --right-of DisplayPort-0
