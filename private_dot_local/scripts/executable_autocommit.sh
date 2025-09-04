7d09639c7aa1a7332714d8b4e4e9046c8d73515a
BladeWDR <github@ssb-tech.net>
2025-09-03 22:25:44 -0400

    Added a notify function to autocommit.sh

--- private_dot_local/scripts/executable_autocommit.sh
+++ private_dot_local/scripts/executable_autocommit.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
-NOTES_PATH="$(pwd)"
+notify(){
+   if command -v notify-send &>/dev/null; then
+      notify-send "Autocommit.sh" "$1"
+      echo "$1"
+   elif command -v wsl-notify-send &>/dev/null; then
+      wsl-notify-send --app-name "Autocommit.sh" "$1"
+   else
+      echo "$1"
+   fi
+}
+
+if [[ -z "$1" ]]; then
+   NOTES_PATH="$(pwd)"
+elif [[ -d "$1" ]]; then
+  NOTES_PATH="$1" 
+else
+   notify "ERROR: unable to determine notes path."
+fi
+
 DATESTAMP="$(date '+%Y-%m-%d %k:%M')"
-cd "$NOTES_PATH"
+cd "$NOTES_PATH" || exit
 
 if ! git rev-parse --is-inside-work-tree &>/dev/null; then
-    echo "Not a Git repository: $NOTES_PATH"
+    notify "Not a Git repository: $NOTES_PATH"
     exit 1
 fi
 
 GIT_STATUS=$(git status --porcelain=v1)
 
-if [[ ! -z $GIT_STATUS ]]; then
+if [[ -n $GIT_STATUS ]]; then
    git pull --rebase --quiet
    git add --all
    git commit -m "AUTOCOMMIT - $DATESTAMP - $HOSTNAME" 
