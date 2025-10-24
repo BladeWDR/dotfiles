9ebb01b29d04777e7059cabd3f4326aa71968fea
BladeWDR <github@ssb-tech.net>
2025-10-24 13:06:33 -0400

    added a remap to set columns to 72

--- private_dot_config/nvim/lua/config/set.lua
+++ private_dot_config/nvim/lua/config/set.lua
@@ -90,3 +90,6 @@ vim.o.foldcolumn = "1" -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+
+-- Set a nice 72 column layout for writing.
+vim.keymap.set({ "n" }, "<leader>7", ":setlocal columns=72<enter>", { desc = "Set 72 columns wrapping." })
