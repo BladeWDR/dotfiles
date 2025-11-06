7f56639358c5d6f20f5a3a48bedabe8123fb6a14
BladeWDR <github@ssb-tech.net>
2025-11-06 13:28:36 -0500

    replace outline.nvim with toc.nvim

---
+++ private_dot_config/nvim/lua/plugins/toc.lua
@@ -0,0 +1,8 @@
+return {
+	"yousefakbar/toc.nvim",
+	ft = "markdown",
+	opts = {},
+	keys = { -- Optional if you want to define a keymap to launch :Toc
+		{ "<leader>O", "<CMD>Toc<CR>", desc = "Table of contents" },
+	},
+}
