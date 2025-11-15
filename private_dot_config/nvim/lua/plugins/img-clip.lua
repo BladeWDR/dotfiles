c029334b9dc4da9e42b6884669657268970966b6
BladeWDR <github@ssb-tech.net>
2025-11-15 10:03:17 -0500

    updated img-clip.lua

--- private_dot_config/nvim/lua/plugins/img-clip.lua
+++ private_dot_config/nvim/lua/plugins/img-clip.lua
@@ -1,12 +1,14 @@
 return {
-  "HakonHarnes/img-clip.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- add options here
-    -- or leave it empty to use the default settings
-  },
-  keys = {
-    -- suggested keymap
-    { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
-  },
+	"HakonHarnes/img-clip.nvim",
+	event = "VeryLazy",
+	opts = {
+		default = {
+			dir_path = "docs/assets/images",
+			use_absolute_path = false,
+			prompt_for_file_name = false,
+		},
+	},
+	keys = {
+		{ "<leader>I", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+	},
 }
