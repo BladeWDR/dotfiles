dfeeb8a12f216cb8705bc1fe6f69800273992e0b
BladeWDR <github@ssb-tech.net>
2025-11-26 09:11:17 -0500

    feat: added TODO.md snippet.

--- private_dot_config/nvim/snippets/all.lua
+++ private_dot_config/nvim/snippets/all.lua
@@ -2,12 +2,45 @@ local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
+local rep = require("luasnip.extras").rep
+local fmt = require("luasnip.extras.fmt").fmt
 
 return {
 	s("ts", {
 		t("#T="),
 		i(1),
 		t(" #S="),
-		i(2), -- No "XX" default
+		i(2),
 	}),
+	s(
+		"todo",
+		fmt(
+			[[
+---
+id: <> To Do List
+aliases: []
+tags:
+  - to do
+  - project
+---
+# <> To Do List
+
+<>
+
+### Todo
+
+### In Progress
+
+### Done ✓
+]],
+			{
+				i(1, "PROJECT_NAME"),
+				rep(1), -- Repeats insert node 1
+				i(2, "PROJECT_DESCRIPTION"),
+			},
+			{
+				delimiters = "<>",
+			}
+		)
+	),
 }
