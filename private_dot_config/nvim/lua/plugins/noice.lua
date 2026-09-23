a3885c7d89383bebeabc6ab47718dd6562f2ab31
BladeWDR <scott@ssb-tech.net>
2026-09-23 14:55:17 -0400

    neovim: make noice.nvim windows not focusable.

--- private_dot_config/nvim/lua/plugins/noice.lua
+++ private_dot_config/nvim/lua/plugins/noice.lua
@@ -13,6 +13,7 @@ return {
 				["vim.lsp.util.stylize_markdown"] = true,
 				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
 			},
+			focusable = false,
 		},
 		-- you can enable a preset for easier configuration
 		presets = {
